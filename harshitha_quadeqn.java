356aa20e59a539a35de441719d509c23fa8670a8
Harshitha R <98110292+HarshithaR-75@users.noreply.github.com>
2022-11-21 19:52:10 +0530

    Update harshitha_quadeqn.java

--- harshitha_quadeqn.java
+++ harshitha_quadeqn.java
@@ -12,6 +12,12 @@ class box
   double d=(b*b)-(4*a*c);
   double r1;
   double r2;
+  if(a==0)
+  {
+   System.out.println("Enter a valid value");
+  }
+  else
+  {
   if(d>0)
   {
     r1=(-b+Math.sqrt(d))/(2*a);
